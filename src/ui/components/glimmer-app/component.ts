98cc47f921760b2c4a7974928e31fd1b106c4f1d
Ken Snyder <ken@ken.net>
2017-06-21 14:25:22 -0700

    simplify

--- src/ui/components/glimmer-app/component.ts
+++ src/ui/components/glimmer-app/component.ts
@@ -10,22 +10,12 @@ export default class GlimmerApp extends Component {
     this.email = (evt.target as HTMLInputElement).value;
   }
 
-  nameUpdated(evt: KeyboardEvent) {
-    this.name = (evt.target as HTMLInputElement).value;
-  }
-
-  changeProvider(value: string, evt: Event) {
+  changeProvider(value: string) {
     this.provider = value;
   } 
 
   sizeUpdated(evt: KeyboardEvent) {
     this.size = Number((evt.target as HTMLInputElement).value);
-  }
-
-  @tracked('name')
-  get initials() {
-    const name = this.name || '';
-    return name.split(' ').map(n => n.slice(0,1).toUpperCase()).join('');
   }
 
   @tracked('provider')
